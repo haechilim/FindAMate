4ecf3e96983a153c454a13806465c3757f0e5d13
임준형 <haechilim@gmail.com>
2021-06-03 23:43:31 +0900

    log 화면 디자인 변경 중...

--- app/src/main/java/com/example/findamate/adapter/LogAdapter.java
+++ app/src/main/java/com/example/findamate/adapter/LogAdapter.java
@@ -5,10 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.findamate.R;
+import com.example.findamate.domain.Couple;
 import com.example.findamate.domain.History;
+import com.example.findamate.domain.Student;
+import com.example.findamate.helper.CoupleView;
+import com.example.findamate.helper.StudentView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -47,6 +52,22 @@ public class LogAdapter extends BaseAdapter {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
 
         ((TextView) view.findViewById(R.id.date)).setText(simpleDateFormat.format(histories.get(position).getCalendar().getTimeInMillis()));
+        LinearLayout container = view.findViewById(R.id.couplesContainer);
+        LinearLayout coupleContainer = new LinearLayout(context);
+        LinearLayout.LayoutParams coupleViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        History history = histories.get(position);
+
+        for(int j = 0; j < history.getCouples().size(); j++) {
+            Couple couple = history.getCouples().get(j);
+            Student student1 = couple.getStudent1();
+            Student student2 = couple.getStudent2();
+
+            CoupleView coupleView = new CoupleView(context, student1, student2);
+            coupleContainer.addView(coupleView, coupleViewParams);
+        }
+
+        container.addView(coupleContainer);
 
         return view;
     }
---
+++ app/src/main/java/com/example/findamate/helper/CoupleView.java
@@ -0,0 +1,48 @@
+package com.example.findamate.helper;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.example.findamate.R;
+import com.example.findamate.domain.Student;
+
+public class CoupleView extends LinearLayout {
+    private Student student1;
+    private Student student2;
+
+    public CoupleView(Context context, Student student1, Student student2) {
+        super(context);
+        this.student1 = student1;
+        this.student2 = student2;
+        init(context);
+    }
+
+    public CoupleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public CoupleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public CoupleView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layoutInflater.inflate(R.layout.layout_couple, this, true);
+
+        StudentView nameOfStudent1 = findViewById(R.id.nameOfStudent1);
+        ((TextView) nameOfStudent1.findViewById(R.id.name)).setText(student1.getId() + " " + student1.getName());
+        //TextView nameOfStudent2 = findViewById(R.id.nameOfStudent2);
+    }
+}
