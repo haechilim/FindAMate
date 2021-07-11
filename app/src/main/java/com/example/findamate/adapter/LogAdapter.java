4613eae631f5ac599b1d446a67863fbb6eef6a4c
임준형 <haechilim@gmail.com>
2021-07-11 22:21:34 +0900

    좋아요, 다시해요 log에서 확인 가능하도록 수정

--- app/src/main/java/com/example/findamate/adapter/LogAdapter.java
+++ app/src/main/java/com/example/findamate/adapter/LogAdapter.java
@@ -69,6 +69,7 @@ public class LogAdapter extends BaseAdapter {
         if(isPoll) {
             rootView.findViewById(R.id.decoration).setVisibility(View.GONE);
             rootView.findViewById(R.id.date).setVisibility(View.GONE);
+            rootView.findViewById(R.id.poll).setVisibility(View.GONE);
         }
 
         LinearLayout couplesRow = null;
@@ -82,6 +83,8 @@ public class LogAdapter extends BaseAdapter {
         }
 
         ((TextView) rootView.findViewById(R.id.date)).setText(formatDate(history));
+        ((TextView) rootView.findViewById(R.id.agree)).setText(String.format("좋아요 %s%%", history.getAgree()));
+        ((TextView) rootView.findViewById(R.id.disagree)).setText(String.format("다시해요 %s%%", 100 - history.getAgree()));
 
         return rootView;
     }
